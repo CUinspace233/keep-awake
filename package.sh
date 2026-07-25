08e6b51f7bee5272477617d2e4784352b5386802
Henrick Lin <3098568188@qq.com>
2026-07-25 23:49:35 +0800

    build: bump VERSION to 1.2

--- package.sh
+++ package.sh
@@ -13,7 +13,7 @@ set -euo pipefail
 
 HERE="$(cd "$(dirname "$0")" && pwd)"
 APP_NAME="KeepAwake"
-VERSION="1.1"
+VERSION="1.2"
 APP_SRC="$HOME/Applications/${APP_NAME}.app"
 DMG_DIR="$HERE/build"
 DMG_NAME="${APP_NAME}-v${VERSION}"
